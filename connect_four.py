8930d13a1c2ab6171e9a6cddbd17e8d9e19b9897
Ryan Ottney <rottney123@gmail.com>
2024-06-13 17:55:26 -0400

    another refactor

--- connect_four.py
+++ connect_four.py
@@ -1,13 +1,20 @@
+import os
+from termcolor import colored
 from copy import deepcopy
+
 
 NUM_COLUMNS = 7
 NUM_ROWS = 6
-BLANK_CELL = '<>'
+BLANK_CELL = colored("o", "blue")
 PLAYER_COLORS = {
-    1: 'R',
-    2: 'Y'
+    1: colored("1", "red"),
+    2: colored("2", "yellow")
 }
 BLANK_BOARD = [[BLANK_CELL for _ in range(NUM_COLUMNS)] for _ in range(NUM_ROWS)]
+
+def cls():
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 class Game:
     def __init__(self):
@@ -17,43 +24,57 @@ class Game:
         self.history = [self.board]
 
     def animate(self):
-        print()
+        cls()
+
         for row in self.board:
-            print('\t'.join(cell for cell in row))
-        print()
+            print("\t".join(cell for cell in row))
 
-    def has_consecutive_k(self, k):
-        for i in range(NUM_ROWS):
-            for j in range(NUM_COLUMNS):
-                if self._check_consecutive(i, j, 0, 1, k) or \
-                   self._check_consecutive(i, j, 1, 0, k) or \
-                   self._check_consecutive(i, j, 1, 1, k) or \
-                   self._check_consecutive(i, j, 1, -1, k):
-                    return True, PLAYER_COLORS[self.current_player]
-        return False, ''
-
-    def _check_consecutive(self, x, y, dx, dy, k):
-        current = self.board[x][y]
-        for i in range(1, k):
-            new_x, new_y = x + i * dx, y + i * dy
-            if not (0 <= new_x < NUM_ROWS and 0 <= new_y < NUM_COLUMNS) or self.board[new_x][new_y] != current:
-                return False
-        return current in PLAYER_COLORS.values()
-
-    def validate_column(self, col):
-        while col == '' or col not in ''.join([str(i) for i in range(1, NUM_COLUMNS + 1)]):
-            col = input(f'Enter a number between 1 and {NUM_COLUMNS}: ')
-
-        return int(col) - 1
+        print(f"\nPlayer {self.current_player} turn")
+    
+    def swap_player(self):
+        self.current_player = 2 if self.current_player == 1 else 1
 
     def undo(self):
         if self.history == [BLANK_BOARD]:
-            print('There is no history to undo.')
+            print("There is no history to undo.")
         else:
             self.history.pop()
             self.board = self.history[-1]
+            self.swap_player()
             self.animate()
-            game.current_player = 2 if game.current_player == 1 else 1
+
+    def has_consecutive_k(self, k):
+        def check_consecutive(x, y, dx, dy, k):
+            current = self.board[x][y]
+
+            for i in range(1, k):
+                new_x, new_y = x + i * dx, y + i * dy
+
+                if not (0 <= new_x < NUM_ROWS and 0 <= new_y < NUM_COLUMNS) or \
+                        self.board[new_x][new_y] != current:
+                    return False
+                
+            return current in PLAYER_COLORS.values()
+        
+        for i in range(NUM_ROWS):
+            for j in range(NUM_COLUMNS):
+                if check_consecutive(i, j, 0, 1, k) or \
+                   check_consecutive(i, j, 1, 0, k) or \
+                   check_consecutive(i, j, 1, 1, k) or \
+                   check_consecutive(i, j, 1, -1, k):
+                    return True
+                
+        return False
+
+    def validate_column(self, col):
+        while col == "" or col not in "".join([str(i) for i in range(1, NUM_COLUMNS + 1)]):
+            col = input(f"Enter a number between 1 and {NUM_COLUMNS}: ")
+
+            if col.lower() == "undo":
+                self.undo()
+
+        return int(col) - 1
+
 
 def play(game):
     game.animate()
@@ -61,15 +82,16 @@ def play(game):
     while not game.game_over:
         game.board = game.history[-1]
 
-        col = input(f'Player {game.current_player} - enter column: ')
-        while col.lower() == 'undo':
+        col = input(f"Player {game.current_player} - enter column: ")
+        while col.lower() == "undo":
             game.undo()
-            col = input(f'Player {game.current_player} - enter column: ')
+            col = input(f"Player {game.current_player} - enter column: ")
         
         col = game.validate_column(col)
-
         while game.board[0][col] != BLANK_CELL:
-            col = input(f'Column {col} is already full. Please choose another column: ')
+            col = input(f"Column {col} is already full. Please choose another column: ")
+            if col.lower() == "undo":
+                game.undo()
             col = game.validate_column(col)
 
         row = NUM_ROWS - 1
@@ -78,18 +100,17 @@ def play(game):
         
         current_board = deepcopy(game.board)
         game.history[-1] = current_board
-
         game.board[row][col] = PLAYER_COLORS[game.current_player]
-
         game.history.append(game.board)
-        game.animate()
 
         result = game.has_consecutive_k(4)
-        if result[0]:
-            print(f'Player {game.current_player} wins!')
+        if result:
+            game.animate()
+            print(f"Player {game.current_player} wins!")
             game.game_over = True
-
-        game.current_player = 2 if game.current_player == 1 else 1
+        else:
+            game.swap_player()
+            game.animate()
 
 
 if __name__ == "__main__":
